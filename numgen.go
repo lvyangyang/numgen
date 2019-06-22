47ca75734b1fa510531a7c03bc557423a5928a9d
lvyy <1667402135@qq.com>
2019-06-22 17:21:54 +0800

    指定长度

--- numgen.go
+++ numgen.go
@@ -10,9 +10,36 @@ func PseudoEncrypt(seed uint32) uint32 {
 	for i := 0; i < 3; i++ {
 		l2 = r1
 		r2 = l1 ^ uint32(math.Round(((float64((1366*r1+150889)%714025))/714025.0)*32767))
-		//r2 = l1 ^ int32(((float64((1366*r1+150889)%714025))/714025.0)*32767)
 		l1 = l2
 		r1 = r2
 	}
 	return uint32((r1 << 16) + l1)
+}
+
+func PseudoEncrypt_v2(seed uint32, dec_len int) uint32 {
+	var bit_len uint32
+	bit_len = 32
+	if math.Pow10(dec_len) >= float64(math.MaxUint32) {
+		bit_len = 32
+	} else {
+		value := int32(math.MaxInt32)
+		for int32(math.Pow10(dec_len)) < value>>2 {
+			value >>= 2
+			bit_len -= 2
+		}
+	}
+
+	var rem_bit uint32 = 32 - bit_len
+	var half_max uint32 = 0xffff >> (rem_bit / 2)
+	var l1, r1 uint32
+	l1 = (seed >> (bit_len / 2)) & uint32(half_max)
+	r1 = seed & uint32(half_max)
+	var l2, r2 uint32
+	for i := 0; i < 3; i++ {
+		l2 = r1
+		r2 = l1 ^ uint32(math.Round(((float64((1366*r1+150889)%714025))/714025.0)*float64(half_max)))
+		l1 = l2
+		r1 = r2
+	}
+	return uint32((l1 << (bit_len / 2)) + r1)
 }
--- numgen_test.go
+++ numgen_test.go
@@ -1,6 +1,8 @@
 package numgen
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -17,5 +19,23 @@ func TestNameGenerate(t *testing.T) {
 			So(dup, ShouldBeFalse)
 			exists[key] = true
 		}
+	})
+}
+
+func TestNameGenerate_v2(t *testing.T) {
+	Convey("pseduencrypt", t, func() {
+		var i uint32
+		for len := 10; len > 0; len-- {
+			exists := make(map[uint32]bool)
+			for i = 0; i < 100000 && i < uint32(math.Pow10(len)); i++ {
+				key := PseudoEncrypt_v2(i, len)
+				_, dup := exists[key]
+				So(dup, ShouldBeFalse)
+				So(key, ShouldBeLessThan, math.Pow10(len+1))
+				exists[key] = true
+				fmt.Println(i, key)
+			}
+		}
+
 	})
 }
